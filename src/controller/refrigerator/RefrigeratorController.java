39b2eca94c3bc25c1757c00e19ba6715018463b4
hyosunLee <leecrossun@gmail.com>
2020-11-17 22:16:48 +0900

    Controller init

---
+++ src/controller/refrigerator/RefrigeratorController.java
@@ -0,0 +1,16 @@
+package controller.refrigerator;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import controller.Controller;
+
+public class RefrigeratorController implements Controller{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
