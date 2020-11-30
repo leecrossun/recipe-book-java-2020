eaa203d810a84a55af588c0350e0abbdebdf18f1
JungEunPyo <sharron315@naver.com>
2020-11-30 22:31:05 +0900

    아마도 last

--- src/controller/recipe/DeleteRecipeController.java
+++ src/controller/recipe/DeleteRecipeController.java
@@ -7,11 +7,12 @@ import javax.servlet.http.HttpSession;
 import controller.Controller;
 import controller.user.UserSessionUtils;
 import persistence.dao.RecipeDAO;
+import persistence.dao.ReviewDAO;
 
 public class DeleteRecipeController implements Controller{
 	
 	private RecipeDAO recipeDAO = new RecipeDAO();
-	
+	private ReviewDAO reviewDAO = new ReviewDAO();
 	public DeleteRecipeController() {
 		try {
 			recipeDAO = new RecipeDAO();
@@ -24,12 +25,15 @@ public class DeleteRecipeController implements Controller{
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		String recipeId = request.getParameter("recipeId");
 		String userId = request.getParameter("userId");
+		recipeDAO.deleteRecipeStep(recipeId);
+		recipeDAO.deleteRecipeIngredient(recipeId);
+		reviewDAO.deleteByRecipeId(recipeId);
 		recipeDAO.deleteRecipe(recipeId);
 //		if (UserSessionUtils.isAdminUser(session))
 //			return "redirect:/recipe/findByRecipe2";
 //		else
 		//return "redirect:/refrigerator/view.jsp";
-		return "redirect:/recipe/myList?userId="+userId;
+		return "/recipe/myList";
 		
 //		HttpSession session = request.getSession();	
 //		
